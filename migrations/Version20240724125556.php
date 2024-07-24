e94e1158c6db1b451add7331469a6c72a04f63dc
alexis-rochefort <alexis.rochefort35@gmail.com>
2024-07-24 16:04:11 +0200

    je suis daccord

--- migrations/Version20240724125556.php
+++ migrations/Version20240724125556.php
@@ -10,7 +10,11 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
+<<<<<<<< Updated upstream:migrations/Version20240724125556.php
 final class Version20240724125556 extends AbstractMigration
+========
+final class Version20240724135923 extends AbstractMigration
+>>>>>>>> Stashed changes:migrations/Version20240724135923.php
 {
     public function getDescription(): string
     {
@@ -21,11 +25,27 @@ final class Version20240724125556 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE applaudissement (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(50) NOT NULL, intensite DOUBLE PRECISION NOT NULL, commentaire VARCHAR(200) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+<<<<<<<< Updated upstream:migrations/Version20240724125556.php
+========
+        $this->addSql('CREATE TABLE botte (id INT AUTO_INCREMENT NOT NULL, pointure INT NOT NULL, couleur VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE croc (id INT AUTO_INCREMENT NOT NULL, hauteur DOUBLE PRECISION NOT NULL, largeur DOUBLE PRECISION NOT NULL, aiguiser TINYINT(1) NOT NULL, lime TINYINT(1) NOT NULL, poli TINYINT(1) NOT NULL, nom_proprietaire VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `cube` (id INT AUTO_INCREMENT NOT NULL, arete DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sarbacane (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, longueur INT NOT NULL, projectile_type VARCHAR(50) NOT NULL, matiere VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+>>>>>>>> Stashed changes:migrations/Version20240724135923.php
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE applaudissement');
+<<<<<<<< Updated upstream:migrations/Version20240724125556.php
+========
+        $this->addSql('DROP TABLE botte');
+        $this->addSql('DROP TABLE croc');
+        $this->addSql('DROP TABLE `cube`');
+        $this->addSql('DROP TABLE sarbacane');
+        $this->addSql('DROP TABLE messenger_messages');
+>>>>>>>> Stashed changes:migrations/Version20240724135923.php
     }
 }
